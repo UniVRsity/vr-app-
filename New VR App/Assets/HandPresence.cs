27b779f994a8cde4d46ae5b30bda21f4749bfd30
Nicolas Barone <njb227@cornell.edu>
2020-09-09 19:37:41 -0400

    ud

--- New VR App/Assets/HandPresence.cs
+++ New VR App/Assets/HandPresence.cs
@@ -3,17 +3,37 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR;
 
-public class test : MonoBehaviour
+public class HandPresence : MonoBehaviour
 {
+    private InputDevice targetDevice;
+    public List<GameObject> controllerPrefabs;
+    private GameObject spawnController;
     // Start is called before the first frame update
     void Start()
     {
       List<InputDevice> devices = new List<InputDevice>();
-      InputDevices.GetDevices(devices);
+      InputDeviceCharacteristics rightControllerCharacteristics = InputDeviceCharacteristics.Right | InputDeviceCharacteristics.Controller;
+      InputDevices.GetDevicesWithCharacteristics(rightControllerCharacteristics, devices);
 
       foreach( var item in devices)
       {
           Debug.Log(item.name+item.characteristics);
+      }
+
+      if (devices.Count > 0)
+      {
+        targetDevice = devices[0];
+        GameObject prefab = controllerPrefabs.Find(controller => controller.name == targetDevice.name);
+        if (prefab)
+        {
+          spawnController = Instantiate(prefab, transform);
+        }
+        else
+        {
+          Debug.LogError("Did not find corresponding controller model");
+          spawnController = Instantiate(controllerPrefabs[0], transform);
+
+        }
       }
 
     }
@@ -21,6 +41,20 @@ public class test : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (targetDevice.TryGetFeatureValue(CommonUsages.primaryButton, out bool primaryButtonValue) && primaryButtonValue)
+        {
+          Debug.Log("Primary Button is pressed");
+        }
         
+        if (targetDevice.TryGetFeatureValue(CommonUsages.trigger, out float triggerValue))
+        {
+          Debug.Log(triggerValue);
+        }
+        
+        if(targetDevice.TryGetFeatureValue(CommonUsages.primary2DAxis, out Vector2 primary2DAxisValue))
+        {
+          Debug.Log("primary touch pad value" + primary2DAxisValue);
+        }
+
     }
 }
--- Uploader Test/Assets/Plugins/uploadKit.bat
+++ Uploader Test/Assets/Plugins/uploadKit.bat
@@ -1,2 +1,2 @@
 echo off
-curl -v -b cookie -X PUT -F "kit[zip]=@C:\Users\Nicolas\Desktop\VR Projects\vr-app-\Uploader Test\1457438402138865951_tingle.zip" -F "kit[game_engine_version]=20194" https://account.altvr.com/api/kits/1457438402138865951.json
+curl -v -b cookie -X PUT -F "kit[zip]=@C:\Users\Nicolas\Desktop\VR Projects\vr-app-\Uploader Test\1457438402138865951_dfsdf.zip" -F "kit[game_engine_version]=20194" https://account.altvr.com/api/kits/1457438402138865951.json
