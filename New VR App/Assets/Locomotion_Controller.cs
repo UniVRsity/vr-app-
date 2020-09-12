d4e444ac3f91e03fd3a9c1f28b6fd0f717eef280
Nicolas Barone <njb227@cornell.edu>
2020-09-11 23:00:10 -0400

    snap turning

---
+++ New VR App/Assets/Locomotion_Controller.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+public class Locomotion_Controller : MonoBehaviour
+{
+    public XRController leftTeleportRay;
+    public XRController rightTeleportRay;
+    public InputHelpers.Button teleportActivationButton;
+    public float activationThreshold = 0.2f;
+
+    
+    public bool checkIfActivated(XRController controller)
+    {
+        InputHelpers.IsPressed(controller.inputDevice, teleportActivationButton, out bool isActivated, activationThreshold);
+        return isActivated;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (leftTeleportRay)
+        {
+            if (checkIfActivated(leftTeleportRay))
+            {
+                StartCoroutine("DelayLeft");
+            }
+        }
+        if (rightTeleportRay)
+        {
+        if (checkIfActivated(rightTeleportRay))
+        {
+            StartCoroutine("DelayRight");
+        }
+    }
+}
+
+IEnumerator DelayLeft()
+{
+    yield return new WaitForSeconds(0.01f);
+    leftTeleportRay.gameObject.SetActive(checkIfActivated(leftTeleportRay));
+}
+
+IEnumerator DelayRight()
+{
+    yield return new WaitForSeconds(0.01f);
+    rightTeleportRay.gameObject.SetActive(checkIfActivated(rightTeleportRay));
+}
+
+}
