0699abe420e1386e54bd445906673fb9fa877d77
Nicolas Barone <njb227@cornell.edu>
2020-09-16 10:02:29 -0400

    sda

--- Uploader Test/Assets/Plugins/uploadKit.bat
+++
@@ -1,2 +0,0 @@
-echo off
-curl -v -b cookie -X PUT -F "kit[zip]=@C:\Users\Nicolas\Desktop\VR Projects\vr-app-\Uploader Test\1457438402138865951_dfsdf.zip" -F "kit[game_engine_version]=20194" https://account.altvr.com/api/kits/1457438402138865951.json
