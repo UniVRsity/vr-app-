871d7bb6936e76a91bfd1889069914b23ee34da0
Nicolas Barone <njb227@cornell.edu>
2020-09-29 01:40:55 -0400

    Criso fixed anim

--- Info Session/Assets/Plugins/uploadKit.bat
+++ Info Session/Assets/Plugins/uploadKit.bat
@@ -1,2 +1,2 @@
 echo off
-curl -v -b cookie -X PUT -F "kit[zip]=@C:\Users\Nicolas\Desktop\VR Projects\vr-app-\Info Session\1561832827287240937_wbanimassign.zip" -F "kit[game_engine_version]=20194" https://account.altvr.com/api/kits/1561832827287240937.json
+curl -v -b cookie -X PUT -F "kit[zip]=@C:\Users\Nicolas\Desktop\VR Projects\vr-app-\Info Session\1561832827287240937_test-folder.zip" -F "kit[game_engine_version]=20194" https://account.altvr.com/api/kits/1561832827287240937.json
