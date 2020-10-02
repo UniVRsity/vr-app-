ccd41106bc0bcbe33ccb71a766b794e6c08f3fb5
Nicolas Barone <njb227@cornell.edu>
2020-10-02 15:22:43 -0400

    moved folders to seperate ones

--- Info Session/Assets/Plugins/signin.bat
+++
@@ -1,3 +0,0 @@
-echo off
-title Sign In to AltspaceVR
-curl -v -d "user[email]=nicolas.barone@univrsitytech.com&user[password]=070498nb" https://account.altvr.com/users/sign_in.json -c cookie
--- Uploader Test/Assets/Plugins/kit_pages.bat
+++
@@ -1,4 +0,0 @@
-echo ontitle AltspaceVR Kits
-
-curl -v -b cookie https://account.altvr.com/api/kits/my.json -o page1-kits.json
-curl -v -b cookie https://account.altvr.com/api/kits/my.json?page=2 -o page2-kits.json
