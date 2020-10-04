fff616fd6d3260ec193963e4ab2d89100895e57f
Nicolas Barone <njb227@cornell.edu>
2020-10-03 20:06:41 -0400

    lighting fix

---
+++ New VR App/Assets/XROffsetGrabInteractable.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class XROffsetGrabInteractable : XRGrabInteractable
+{
+    // Start is called before the first frame update
+    private Vector3 initialAttachLocalPos;
+    private Quaternion initialAttachLocalRot;
+    void Start()
+    {
+        if (!attachTransform)
+        {
+            GameObject grab = new GameObject("Grab Pivot");
+            grab.transform.SetParent(transform, false);
+            attachTransform = grab.transform;
+        }
+        initialAttachLocalPos = attachTransform.localPosition;
+        initialAttachLocalRot  = attachTransform.localRotation;
+    }
+
+    protected override void OnSelectEnter(XRBaseInteractor interactor)
+    {
+        if (interactor is XRDirectInteractor){
+            attachTransform.position = interactor.transform.position;
+            attachTransform.rotation = interactor.transform.rotation;
+        }
+        base.OnSelectEnter(interactor);
+    }
+}
