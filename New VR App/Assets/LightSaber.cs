625d9d1450330924530bdf6d8fcc41937bbd4490
Nicolas Barone <njb227@cornell.edu>
2020-10-05 15:54:38 -0400

    lightsaber aniamtion

---
+++ New VR App/Assets/LightSaber.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightSaber : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+   private Animator LightSaberAnimator;
+   void Start()
+    {
+        LightSaberAnimator = GetComponent<Animator>();
+    }
+
+    public void GrowLightSaber()
+    {
+        LightSaberAnimator.SetTrigger("GrowSaber");
+        LightSaberAnimator.ResetTrigger("ShrinkSaber");
+    }
+    public void ShrinkLightSaber()
+    {
+        LightSaberAnimator.SetTrigger("ShrinkSaber");
+        LightSaberAnimator.ResetTrigger("GrowSaber");
+    }
+}
